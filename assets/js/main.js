ce86105e23756e32ef13070f29963ad25ee6fa0e
Mohammad <m.alzebaq@gmail.com>
2020-03-16 15:46:23 +0100

    lvl1-4

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+var unsereHaustiere = [
+    {
+        tiertyp: "Katze",
+        names: [
+            "Gipsy",
+            "Nala",
+            "Dinky"
+        ]
+    },
+    {
+        tiertyp: "Hunde",
+        names: [
+            "Knöpfchen",
+            "Pinselchen",
+            "Droopy"
+        ]
+    }
+];
+unsereHaustiere[0].names[1] = "Eva"
+unsereHaustiere[1].names[2] = "Zaza"
+console.log(unsereHaustiere)
